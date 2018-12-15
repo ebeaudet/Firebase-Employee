57cdb476fadf4ae4e4c694652dfb872c527ae0c5
Ronald Glover <ron881212@gmail.com>
2018-12-15 10:34:36 -0500

    working

---
+++ app.js
@@ -0,0 +1,18 @@
+var EmployeeName;
+var Role;
+var StartDate;
+var mounthsWork;
+var MonthlyRate;
+var TotalBilled;
+
+
+
+$(document).on("click", function(){
+    tableRow = $("<tr>");
+    tableData = $("<td>");
+    tableRow.text();
+    tableData.text();
+
+    tableRow.append(tableData);
+    $(tbody).append(tableData);
+})
