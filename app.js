c2a947159e56e14bc1adbf2d84cbea3c6f75a736
Ronald Glover <ron881212@gmail.com>
2018-12-15 11:01:26 -0500

    added firebase

--- app.js
+++ app.js
@@ -7,29 +7,38 @@ var TotalBilled;
 
 // We will need this
 // use .push({}) to add and not change database.ref().on("child_added", function(snapshot) {  
-        // Capture Button Click
-        // $("#add-user").on("click", function(event) {
-        //     event.preventDefault();
-      
-        //     // Grabbed values from text boxes
-        //     name = $("#name-input").val().trim();
-        //     email = $("#email-input").val().trim();
-        //     age = $("#age-input").val().trim();
-        //     comment = $("#comment-input").val().trim();
-      
-        //     // Code for handling the push
-        //     database.ref().push({
-        //       name: name,
-        //       email: email,
-        //       age: age,
-        //       comment: comment,
-        //       dateAdded: firebase.database.ServerValue.TIMESTAMP
-        //     });
-      
-        //   });
+// Capture Button Click
+// $("#add-user").on("click", function(event) {
+//     event.preventDefault();
 
+//     // Grabbed values from text boxes
+//     name = $("#name-input").val().trim();
+//     email = $("#email-input").val().trim();
+//     age = $("#age-input").val().trim();
+//     comment = $("#comment-input").val().trim();
 
-$(document).on("click", ".jumbotron", function(){
+//     // Code for handling the push
+//     database.ref().push({
+//       name: name,
+//       email: email,
+//       age: age,
+//       comment: comment,
+//       dateAdded: firebase.database.ServerValue.TIMESTAMP
+//     });
+
+//   });
+
+var config = {
+    apiKey: "AIzaSyCyOv9lPv3btckAveNO-HRpuIPrTvzi7QM",
+    authDomain: "time-sheets-25e21.firebaseapp.com",
+    databaseURL: "https://time-sheets-25e21.firebaseio.com",
+    projectId: "time-sheets-25e21",
+    storageBucket: "",
+    messagingSenderId: "208307836815"
+};
+firebase.initializeApp(config);
+
+$(document).on("click", ".jumbotron", function () {
     tableRow = $("<tr>");
     tableData1 = $("<td>");
     tableData2 = $("<td>");
@@ -40,7 +49,7 @@ $(document).on("click", ".jumbotron", function(){
     tableRow.text("employer");
     tableData.text("something");
 
-    tableRow.append(tableData);
+    tableRow.append(tableData1, tableData2, tableData3, tableData4, tableData5, tableData6);
     $(tbody).append(tableData);
-    
+
 });
